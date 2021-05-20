cc6cdb97fbdecaf1d3b91931930e7c4e9e7c31bd
yin-turbo <18956009339@163.com>
2021-05-20 16:04:08 +0800

    change

---
+++ connectRedis/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-redis/redis"
+	_ "github.com/go-redis/redis"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// 声明一个全局的rdb变量
+var rdb *redis.Client
+
+// 初始化连接
+func initClient(ctx context.Context) (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "root", // no password set
+		DB:       0,      // use default DB
+	})
+
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func main() {
+	//connect redis
+	ctx := context.Background()
+	err := initClient(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("connect redis successfly")
+
+}
